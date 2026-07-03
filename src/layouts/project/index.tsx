f4cd63053e303c9b88d1b1bd24e300269181c5a4
ZeroFuku <12mnsin@gmail.com>
2026-07-03 13:54:04 +0800

    minor layout fixes and button setup

--- src/layouts/project/index.tsx
+++ src/layouts/project/index.tsx
@@ -7,11 +7,11 @@ function ProjectLayout(){
             <div className="flex flex-col items-center justify-center w-screen h-screen bg-[#17161D]">
                 <Outlet/>
 
-                <div className="flex flex-row items-center justify-center gap-4 fixed bottom-20 left-20">
+                <div className="flex flex-row items-center justify-center gap-4 fixed bottom-20 left-20 z-10">
                     <DocumentationSettings/>
                 </div>
 
-                <div className="flex flex-row items-center justify-center gap-4 fixed bottom-20 right-20">
+                <div className="flex flex-row items-center justify-center gap-4 fixed bottom-20 right-20 z-10">
                     <TeamChatButton/>
                     <ChatBotButton/>
                 </div>
@@ -19,7 +19,6 @@ function ProjectLayout(){
         </main>
     )
 }
-
 function DocumentationSettings(){
     return (
         <div className="p-6 rounded-2xl flex flex-col gap-4 border border-[#645D68] text-white max-w-60">
@@ -50,7 +49,7 @@ function DocumentationSettings(){
 
 function TeamChatButton(){
     return (
-        <button className="flex flex-row gap-2 justify-center items-center text-white p-4 rounded-2xl bg-linear-to-r from-[#FF0D00] via-[#B50F6D] to-[#6C1CD7] cursor-pointer hover:scale-105 transition-all duration-300 group w-fit"> 
+        <button onClick={undefined} className="flex flex-row gap-2 justify-center items-center text-white p-4 rounded-2xl bg-linear-to-r from-[#FF0D00] via-[#B50F6D] to-[#6C1CD7] cursor-pointer hover:scale-105 transition-all duration-300 group w-fit"> 
             <MessageCircle size={40} color="white"/>
             <span className="max-w-0 overflow-hidden whitespace-nowrap transition-all duration-300 group-hover:max-w-xs group-hover:ml-2">
                 Messages
@@ -61,7 +60,7 @@ function TeamChatButton(){
 
 function ChatBotButton(){
     return (
-        <button className="flex flex-row gap-2 justify-center items-center text-white p-4 rounded-2xl bg-linear-to-r from-[#FF0D00] via-[#B50F6D] to-[#6C1CD7] cursor-pointer hover:scale-105 transition-all duration-300 group w-fit"> 
+        <button onClick={undefined} className="flex flex-row gap-2 justify-center items-center text-white p-4 rounded-2xl bg-linear-to-r from-[#FF0D00] via-[#B50F6D] to-[#6C1CD7] cursor-pointer hover:scale-105 transition-all duration-300 group w-fit"> 
             <Bot size={40} color="white"/> 
             <span className="max-w-0 overflow-hidden whitespace-nowrap transition-all duration-300 group-hover:max-w-xs group-hover:ml-2">
                 help me S.I.S.t.e.r im stuck!
@@ -69,4 +68,5 @@ function ChatBotButton(){
         </button>
     )
 }
+
 export default ProjectLayout
