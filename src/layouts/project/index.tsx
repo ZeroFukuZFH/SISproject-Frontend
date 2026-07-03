de8e98dc0dd93833629b6e0938371480e42cb5e8
ZeroFuku <12mnsin@gmail.com>
2026-07-03 13:10:19 +0800

    layout background commit

--- src/layouts/project/index.tsx
+++ src/layouts/project/index.tsx
@@ -3,7 +3,9 @@ import { Outlet } from "react-router";
 function ProjectLayout(){
     return (
         <main>
-            <Outlet/>
+            <div className="flex flex-col items-center justify-center w-screen h-screen bg-[#17161D]">
+                <Outlet/>
+            </div>
         </main>
     )
 }
