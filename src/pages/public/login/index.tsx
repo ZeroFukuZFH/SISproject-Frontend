b71883153568eec71f22f215c444b9ee511327be
ZeroFuku <12mnsin@gmail.com>
2026-07-04 21:44:08 +0800

    initial designs for both login and register completed

--- src/pages/public/login/index.tsx
+++ src/pages/public/login/index.tsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router"
-import Background from "../../../assets/auth_bg.png"
 import LoadingSpinner from "../../../components/LoadingSpinner"
 import useLogin from "./hooks/useLogin/hook"
 import {  ShieldAlert } from "lucide-react"
+import AuthTextInput from "../../../components/AuthTextInput"
 
 function LoginPage() {
     const {login, handleChange, handleSubmit, errorMessage} = useLogin()
     return (
-        <div className="w-screen h-screen flex justify-center items-center">
             <div className="flex flex-col gap-4 rounded-xl bg-[#17161D] border border-[#423D44] w-100 h-140 text-white justify-center items-center p-10 ">
                 <h1 className="text-4xl font-bold">Welcome to S.I.S!</h1>
                 <p className="font-extralight">We’re  so excited to see you again!</p>
@@ -17,19 +16,19 @@ function LoginPage() {
                     <ShieldAlert/> {errorMessage}
                 </div>
                 }
-                <input 
+                <AuthTextInput
+                    status={login.status}
                     type="email"
                     placeholder="email" 
                     name="email" 
-                    className={"text-white rounded-md px-4 py-2 outline-none bg-[#34353B] w-full " + (login.status === 'error' && 'border border-red-500')} 
                     value={login.email}
                     onChange={handleChange}
                 />
-                <input 
+                <AuthTextInput
+                    status={login.status}
                     type="password" 
                     placeholder="password" 
                     name="password" 
-                    className={"text-white rounded-md px-4 py-2 outline-none bg-[#34353B] w-full " + (login.status === 'error' && 'border border-red-500')}
                     value={login.password}
                     onChange={handleChange}
                 />
@@ -41,12 +40,7 @@ function LoginPage() {
                 <span className="w-full flex justify-start gap-2 items-center">Already have an account? <Link to={"/register"} className="text-violet-500">register</Link></span>
 
             </div>
-            <div className="fixed w-full h-full bg-black/50 -z-10"/>
-            <img src={Background} alt="bg_auth" className="flex w-full h-full fixed -z-20"/> 
-        </div>
     )
 }
-
-
 
 export default LoginPage
--- src/pages/public/register/hooks/useRegister/hook.tsx
+++ src/pages/public/register/hooks/useRegister/hook.tsx
@@ -1,36 +1,39 @@
 import { useState, type ChangeEvent } from "react"
 
-type LoginState = {
+type RegisterState = {
+    username: string,
     email: string,
     password: string,
     status: 'success' | 'error' | 'loading' | 'normal'
 }
 
-const defaultLoginState : LoginState = {
+const defaultRegisterState : RegisterState = {
+    username: "",
     email: "",
     password: "",
     status: "normal"
 }
 
-function useLogin(){
-    const [login, setLogin] = useState(defaultLoginState)
+function useRegister(){
+    const [register, setRegister] = useState(defaultRegisterState)
     const [errorMessage, setErrorMessage] = useState("")
     
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const {name,value} = e.target
-        setLogin(prev => ({...prev,[name]:value,status:'normal'}))
+        const { name, value } = e.target
+        
+        setRegister(prev => ({ ...prev, [name]: value, status: 'normal' }))
         setErrorMessage("")
     }
 
     const handleSubmit = async () => {
-        const {email,password} = login
+        const { username, email, password } = register
         
         try {
-            if(email === "" || password === ""){
+            if (username.trim() === "" || email === "" || password === "") {
                 throw new Error("fields should not be empty!")
             }
 
-            if(!email.includes("@") || !email.includes(".")){
+            if (!email.includes("@") || !email.includes(".")) {
                 throw new Error("email must be valid")
             }
 
@@ -41,26 +44,26 @@ function useLogin(){
                 throw new Error("password must be at least 12 characters and contain atleast one special and uppercase character");
             }
             
-            setLogin({...login,status:'loading'})
-
+            setRegister({ ...register, status: 'loading' })
             await new Promise(resolve => setTimeout(resolve, 1000)); 
-
-            setLogin({...login,status:'success'})
+            
+            setRegister({ ...register, status: 'success' })
+            
         } catch (error: unknown) {
-            if(error instanceof Error){
+            if (error instanceof Error) {
                 setErrorMessage(error.message)
-                setLogin({...login, status:'error'})
+                setRegister({ ...register, status: 'error' })
             }
             return
         }
     }
 
     return {
-        login,
+        register,
         handleChange,
         handleSubmit,
         errorMessage
     }
 }
 
-export default useLogin
+export default useRegister
