53528be0bb75a5f0e3ec60ca0b5ff08924a673f0
ZeroFuku <12mnsin@gmail.com>
2026-07-05 19:24:37 +0800

    minor bug fix: forgot to take care of all dependencies for useAuth

--- src/layouts/landing/hooks/useProfilePopup/hook.tsx
+++ src/layouts/landing/hooks/useProfilePopup/hook.tsx
@@ -1,9 +1,9 @@
-import useAuth from "../../../../hooks/useAuth/hook"
+
 import { useState, type ChangeEvent } from "react"
 
 function useProfilePopup(){
-    const { auth } = useAuth()
-    const { email, username } = auth
+    const email = "" // TODO: create response 
+    const username = "" // TODO: create response 
     const [signOut, setSignOut] = useState(false)
     const [activityStatus, setActivityStatus] = useState('available') // TODO: add as response type later ( 'available' | 'away' | 'do not disturb' | 'offline' )
     const handleStatusChange = (e : ChangeEvent<HTMLButtonElement>) => { 
